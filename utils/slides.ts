52ee26422bb42ed290ecf0ee6e7c9ebf4cd998cb
Diary Salah <diarysalah36@gmail.com>
2025-03-06 16:46:38 +0300

    feat: Onboarding pages data

---
+++ utils/slides.ts
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: 1,
+        title: 'Slide 1',
+        description: 'Description 1'
+    },
+    {
+        id: 2,
+        title: 'Slide 2',
+        description: 'Description 2'
+    },
+    {
+        id: 3,
+        title: 'Slide 3',
+        description: 'Description 3'
+    },
+    {
+        id: 4,
+        title: 'Slide 4',
+        description: 'Description 4'
+    },
+]
+
+export default data
