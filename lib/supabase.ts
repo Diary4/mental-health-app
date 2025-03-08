63f4f7ca8ccb7e4bba1b8ac07d423319397b4473
Diary Salah <diarysalah36@gmail.com>
2025-03-08 19:46:57 +0300

    fix: fixing async storage on browser

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,9 +6,10 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = "https://dqijpyguoraipzzndwmu.supabase.co"
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxaWpweWd1b3JhaXB6em5kd211Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1NzM0MzUsImV4cCI6MjA1NjE0OTQzNX0.6EVIAoXALmheQ2OkxLp8hto-WOH8W14Cx8tOIQ5IpWg"
 
+const isWeb = typeof window !== "undefined";
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    storage: isWeb ? window.localStorage : AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
